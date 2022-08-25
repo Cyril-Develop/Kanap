51439b3c92b6c3904dc1bbda087a052397645a67
Cyril bourdon <cyril.bourdon@hotmail.fr>
2022-08-25 23:16:10 +0200

    Modif fonction delete

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,3 @@
-const cartAndFormContainer = document.querySelector('#cartAndFormContainer h1')
-const deletionInfo = document.createElement('p');
-
 const productInStorage = JSON.parse(localStorage.getItem('productSelected'));
 
 if(!productInStorage){
@@ -11,65 +8,99 @@ if(!productInStorage){
         fetch(`http://localhost:3000/api/products/${product.id}`)
             .then(res => res.json())
             .then(data => {
-                if(!productInStorage){
-                    document.querySelector('.cartAndFormContainer h1').innerHTML = 'Votre panier est vide.';
-                    document.querySelector('.cart').style.display = 'none';
-                }else{
-                    let modelCard = ` <article class="cart__item" data-id="${data._id}" data-color="${product.color}">
-                                        <div class="cart__item__img">
-                                        <img src="${data.imageUrl}" alt="Photographie d'un canapé ${data.name}">
+                let modelCard = ` <article class="cart__item" data-id="${data._id}" data-color="${product.color}">
+                                    <div class="cart__item__img">
+                                    <img src="${data.imageUrl}" alt="Photographie d'un canapé ${data.name}">
+                                    </div>
+                                    <div class="cart__item__content">
+                                    <div class="cart__item__content__description">
+                                        <h2>${data.name}</h2>
+                                        <p>${product.color}</p>
+                                        <p>${data.price} €</p>
+                                    </div>
+                                    <div class="cart__item__content__settings">
+                                        <div class="cart__item__content__settings__quantity">
+                                        <p>Qté : </p>
+                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
                                         </div>
-                                        <div class="cart__item__content">
-                                        <div class="cart__item__content__description">
-                                            <h2>${data.name}</h2>
-                                            <p>${product.color}</p>
-                                            <p>${data.price} €</p>
+                                        <div class="cart__item__content__settings__delete">
+                                        <p class="deleteItem">Supprimer</p>
                                         </div>
-                                        <div class="cart__item__content__settings">
-                                            <div class="cart__item__content__settings__quantity">
-                                            <p>Qté : </p>
-                                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                                            </div>
-                                            <div class="cart__item__content__settings__delete">
-                                            <p class="deleteItem">Supprimer</p>
-                                            </div>
-                                        </div>
-                                        </div>
-                                    </article>`;
-                    document.querySelector('#cart__items').innerHTML += modelCard;  
+                                    </div>
+                                    </div>
+                                </article>`;
+                document.querySelector('#cart__items').innerHTML += modelCard; 
                     
-                    //delete quantity
-                    ////////////////////////////////////////////////////////////////////////////
-                    const deleteItem = document.querySelectorAll('.deleteItem');
-                    deleteItem.forEach(item => {
-            
-                        item.addEventListener('click', () => {
-                            // showDeletion();
-                            let produtSelectedForDeletion = item.closest('article');
-                            let idProductToDelete = produtSelectedForDeletion.getAttribute('data-id');
-                            const foundProduct = productInStorage.find(el => el.id === idProductToDelete);
-                            //productInStorage.pop(foundProduct);
-                            //produtSelectedForDeletion.remove();
-                            
-                            localStorage.setItem('productSelected', JSON.stringify(productInStorage));
-                            if(productInStorage.length === 0){
-                                localStorage.clear()
-                            }
-                            alert('Produit retiré du panier')
-                            location.reload(); 
-                            
-                        });
-                        
-                    });
-                    
-                    //show total
-                    ////////////////////////////////////////////////////////////////////////////
-              
-                }
-                           
-        })
+                deleteProduct();
+                getTotalQuantity()      
+            }
+                  
+        )
     }
-};
+};              
+ 
+                    
+                    
+                    
+  
+    
+
+
+
+///////////////////////////////////////////////////////Delete item///////////////////////
+                    
+function deleteProduct(){
+
+    const deleteItem = document.querySelectorAll('.deleteItem');
+
+    deleteItem.forEach(item => {
+
+        item.addEventListener('click', () => {
+            // showDeletion();
+            let produtSelectedForDeletion = item.closest('article');
+            let idProductToDelete = produtSelectedForDeletion.getAttribute('data-id');
+            const foundProduct = productInStorage.find(el => el.id === idProductToDelete);
+            //productInStorage.pop(foundProduct);
+            //produtSelectedForDeletion.remove();
+            localStorage.setItem('productSelected', JSON.stringify(productInStorage));
+            if(productInStorage.length === 0){
+                localStorage.clear()
+            }
+            alert('Produit retiré du panier')
+            location.reload(); 
+     
+        });
+        
+    });
+}
+/////////////////////////////////////Show total quantity/////////////////////////////////////////
+function getTotalQuantity(){
+
+    let allInput = Array.from(document.querySelectorAll('.itemQuantity'))
+
+        console.log(allInput);
+        
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cartAndFormContainer = document.querySelector('#cartAndFormContainer h1')
+// const deletionInfo = document.createElement('p');
 // function showDeletion(){
 //     deletionInfo.innerHTML = 'Produit retiré du panier'
 //     cartAndFormContainer.append(deletionInfo);
