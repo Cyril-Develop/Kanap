8f92eef438773cd59fbb53accc662170453c5242
Cyril bourdon <cyril.bourdon@hotmail.fr>
2024-01-21 09:56:06 +0100

    add dotenv

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+require('dotenv').config();
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
@@ -12,7 +13,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || 3000);
 app.set('port', port);
 
 const errorHandler = error => {
