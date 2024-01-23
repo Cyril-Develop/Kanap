66be0fe7154f69c63f4512a606a47b521994c981
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-01-23 12:21:25 +0100

    contrast + modif title product page => fin

--- api/app.js
+++ api/app.js
@@ -6,7 +6,7 @@ const productRoutes = require('./routes/product');
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://cyril-develop.fr');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
